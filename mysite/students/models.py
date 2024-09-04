f0de6dda9c7b48a9443fe68f03838d91e437554f
nohaashehab <nohashehab.iti@gmail.com>
2024-09-04 11:42:07 +0300

    register model to the admin

--- mysite/students/models.py
+++ mysite/students/models.py
@@ -5,8 +5,14 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
-    track = models.CharField(max_length=100, null=True)
+    # null on db level , blank= => application
+    track = models.CharField(max_length=100, null=True, blank=True)
     image = models.CharField(max_length=100, null=True, blank=True)
     grade = models.IntegerField(default=0)
     email = models.EmailField(max_length=100, unique=True, null=True)
     gender = models.Choices('male', 'female')
+
+
+    # level of the application
+    def __str__(self):
+        return self.name
